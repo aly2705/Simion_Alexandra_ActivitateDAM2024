83fe1c1980bb22ab668c1f2b76d139db6c552c4b
aly2705 <alexandraaly.simion@gmail.com>
2024-10-16 22:17:06 +0300

    adaugare seminar 3

---
+++ Seminar3/app/src/main/java/com/example/seminar3/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.seminar3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void openActivity2(View view) {
+        Intent it = new Intent(this, MainActivity2.class);
+
+        // cheie si val primitiva (altfel un obiect Parsable)
+        it.putExtra("text", "Suma este: ");
+        it.putExtra("number1", 10);
+        it.putExtra("number2", 100);
+        startActivity(it);
+
+    }
+}
---
+++ Seminar3/app/src/main/java/com/example/seminar3/MainActivity2.java
@@ -0,0 +1,34 @@
+package com.example.seminar3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent it = getIntent();
+        String mesaj = it.getStringExtra("text");
+        int suma = it.getIntExtra("number1", 0) + it.getIntExtra("number2", 0);
+
+        Toast.makeText(this, mesaj + suma, Toast.LENGTH_LONG).show();
+    }
+
+
+}
